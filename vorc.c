a4f27d8123286452651a041294148770a38c4510
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-01-11 11:20:22 +0000

    Create vorc.c

---
+++ vorc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch
+printf("enter an alphabet:\n");
+scanf("%c",&ch);
+if(ch=='a')
+{
+printf("\n%c is a vowels",ch);
+}
+else
+{
+printf("\n%c is a constant",ch);
+}
+return 0;
+}
