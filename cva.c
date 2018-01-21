6e0fc7834ae2d26974a73dc99ba1acc13de2cde9
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-01-21 08:01:07 +0000

    Create cva.c

---
+++ cva.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int flag=0,i,n;
+printf("Enter the positive integer");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n/i==o)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("%d  prime number");
+}
+else
+{
+printf("%d  not a prime number);
+}
+return 0;
+}
+}
