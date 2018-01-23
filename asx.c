b0b01bae8082ef0eb2c0f09c84cb2f2992ca19cb
sivasmart007 <31505705+sivasmart007@users.noreply.github.com>
2018-01-23 11:18:20 +0000

    Update asx.c

--- asx.c
+++ asx.c
@@ -1,2 +1,14 @@
-wsssdcc
-hjg
+#include<stdio.h>
+int main()
+{
+int n,k,diff,sum=0,temp,i;
+scanf("%d%d%d",&n,&k,&diff);
+for(i=0;i<n;i++)
+{
+temp=sum+k;
+sum=temp;
+k=k+diff;
+}
+printf("%d",temp);
+return 0;
+}
